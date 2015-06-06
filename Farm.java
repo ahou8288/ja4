0ad94706348460f2e9b93baa3501002bd000c730
ahou8288 <ahou8288@uni.sydney.edu.au>
2015-06-06 10:04:05 +1000

    Update Farm.java

--- Farm.java
+++ Farm.java
@@ -6,7 +6,7 @@
 
 public class Farm {
 
-    private Crop[][] crops;
+    public Crop[][] crops;
     
 	private int height=0;
 	private int width=0;
@@ -14,6 +14,22 @@ public class Farm {
 	
 	public int getCoins() {
 		return coins;
+	}
+	
+	public boolean payCoins(int payment){
+	    if (payment<0){
+    		System.out.printf("Collect %d coins\n",-payment);
+			coins-=payment;
+			return true;
+	    } else {
+    		if (coins>=payment){
+    			coins-=payment;
+    			System.out.printf("Cost %d coins\n",payment);
+    			return true;
+    		} else {
+    			return false;
+    		}
+	    }
 	}
 	
 	public int getHeight() {
@@ -62,11 +78,19 @@ public class Farm {
 	public void showSprayed() {
 		for (int i=0;i<getHeight();i++){
 			for (int j=0;j<getWidth();j++){
-				if (j==getWidth()-1){
-					System.out.printf("%d",crops[j][i].isSprayed()?1:0);
-				}else{
-					System.out.printf("%d ",crops[j][i].isSprayed()?1:0);
-				}
+			    if (crops[j][i].isSprayed()){
+    				if (j==getWidth()-1){
+    					System.out.printf("%s","+");
+    				}else{
+    					System.out.printf("%s ","+");
+    				}
+			    } else{
+			        if (j==getWidth()-1){
+    					System.out.printf("%s",".");
+    				}else{
+    					System.out.printf("%s ",".");
+    				}
+			    }
 			}
 			System.out.printf("\n");
 		}
@@ -74,21 +98,52 @@ public class Farm {
 	public void showInfected() {
 		for (int i=0;i<getHeight();i++){
 			for (int j=0;j<getWidth();j++){
-				
-				if (j==getWidth()-1){
-					System.out.printf("%d",crops[j][i].isInfested()?1:0);
-				}else{
-					System.out.printf("%d ",crops[j][i].isInfested()?1:0);
-				}
+				if (crops[j][i].isInfested()){
+    				if (j==getWidth()-1){
+    					System.out.printf("%s","x");
+    				}else{
+    					System.out.printf("%s ","x");
+    				}
+			    } else{
+			        if (j==getWidth()-1){
+    					System.out.printf("%s",".");
+    				}else{
+    					System.out.printf("%s ",".");
+    				}
+			    }
 			}
 			System.out.printf("\n");
 		}
 	}
 	public void grow() {
+	    //Create an identical farm
+	    Farm newFarm=new Farm(getHeight(),getWidth());
 		for (int i=0;i<getHeight();i++){
 			for (int j=0;j<getWidth();j++){
-				crops[j][i].grow();
+				crops[j][i].clone(newFarm,j,i);
 			}
 		}
+		//Look at the copy to determine the new state
+		for (int i=0;i<getHeight();i++){
+			for (int j=0;j<getWidth();j++){
+				crops[j][i].grow(newFarm,j,i);
+			}
+		}
+	}
+
+	public void waterOne(int x1, int y1) {
+		crops[y1-1][x1-1].setWater(8);
+	}
+	
+	public void sprayOne(int x1,int y1){
+	    crops[y1-1][x1-1].setSprayed(true);
+	}
+	
+	public void infestOne(int x1,int y1){
+	    crops[y1-1][x1-1].setInfested(true);
+	}
+	
+	public void harvestOne(int x1,int y1){
+	    crops[y1-1][x1-1].setHeight(0);
 	}
 }
