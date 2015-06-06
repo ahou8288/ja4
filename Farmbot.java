bd85dd9c392e56f4bf1825c1c8de27bf650fd664
ahou8288 <ahou8288@uni.sydney.edu.au>
2015-06-06 10:03:47 +1000

    Update Farmbot.java

--- Farmbot.java
+++ Farmbot.java
@@ -21,9 +21,26 @@ public class Farmbot {
     // Initialises program based on the command line arguments
     public static void init(String[] args) {
         
-        int width = 3;
-        int height = 3;
-        day=0;
+    	if (args.length!=2){
+    		System.out.printf("Invalid command line arguments\nUsage: java Farmbot <width> <height>\n");
+    		System.exit(0);
+    	}
+    	int width=3;
+    	int height=3;
+    	try{
+	        width = Integer.parseInt(args[0]);
+	        height = Integer.parseInt(args[1]);
+    	}catch (NumberFormatException e){
+    		System.out.printf("Invalid command line arguments\nUsage: java Farmbot <width> <height>\n");
+    		System.exit(0);
+    	}
+    	
+    	if (width<=0||height<=0){
+    		System.out.printf("Invalid command line arguments\nUsage: java Farmbot <width> <height>\n");
+    		System.exit(0);
+    	}
+    	
+        day=1;
 
         // Attempt to parse command line arguments
         // ...
@@ -103,6 +120,7 @@ public class Farmbot {
     public static void nextCommand() {
         day++;
         farm.grow();
+        statusCommand();
     }
     
     // Displays the current day and amount of coins held
@@ -110,7 +128,7 @@ public class Farmbot {
         
         // STATUS
         
-        System.out.println("TODO");
+        System.out.printf("Day %d - %d coins\n",day,farm.getCoins());
     }
     
     // Displays the particular state of the crops
@@ -145,9 +163,6 @@ public class Farmbot {
     
     // Waters a crop plot or region of crop plots
     public static void waterCommand(String[] tokens) {
-
-        // WATER <x1> <y1> [<x2> <y2>]
-        
         if (tokens.length == 3) {
             int plot[] = parsePlot(tokens);
     
@@ -156,11 +171,15 @@ public class Farmbot {
                 return;
             }
     
-            // Water crop plot
-            // - x1, y1 maps to plot[0], plot[1]
-
-            System.out.println("TODO");
-
+            int x1=Integer.parseInt(tokens[1]);
+            int y1=Integer.parseInt(tokens[2]);
+            if(x1<1||y1<1||x1>farm.getHeight()||y1>farm.getWidth()){
+            	System.out.println("Invalid arguments");
+            	return;
+            }
+            farm.waterOne(x1,y1);
+            farm.payCoins(10);
+            
         } else if (tokens.length == 5) {
             int region[] = parseRegion(tokens);
             
@@ -169,11 +188,27 @@ public class Farmbot {
                 return;
             }
             
-            // Water region of crop plots
-            // - x1, y1 maps to region[0], region[1]
-            // - x2, y2 maps to region[2], region[3]
-
-            System.out.println("TODO");
+            int x1=Integer.parseInt(tokens[1]);
+            int y1=Integer.parseInt(tokens[2]);
+            int x2=Integer.parseInt(tokens[3]);
+            int y2=Integer.parseInt(tokens[4]);
+            
+            if(x1<1||y1<1||x1>farm.getHeight()||y1>farm.getWidth()){
+                if(x2<x1||y2<y1||x2>farm.getHeight()||y2>farm.getWidth()){
+	            	System.out.println("Invalid arguments");
+	            	return;
+                }
+            }
+            if (farm.payCoins((x2-x1+1)*(y2-y1+1)*10)){
+            	for (int i=x1;i<=x2;i++){
+                 	for (int j=y1;j<=y2;j++){
+                         farm.waterOne(j,i);
+                 	}
+                 }
+            } else {
+            	System.out.println("Not enough coins");
+            	return;
+            }
 
         } else {
             System.out.println("Invalid arguments");
@@ -183,25 +218,171 @@ public class Farmbot {
     // Sprays a crop plot or region of crop plots
     public static void sprayCommand(String[] tokens) {
         
-        // SPRAY <x1> <y1> [<x2> <y2>]
-        
-        System.out.println("TODO");
+        if (tokens.length == 3) {
+            int plot[] = parsePlot(tokens);
+    
+            if (plot == null) {
+                System.out.println("Invalid arguments");
+                return;
+            }
+    
+            int x1=Integer.parseInt(tokens[1]);
+            int y1=Integer.parseInt(tokens[2]);
+            if(x1<1||y1<1||x1>farm.getHeight()||y1>farm.getWidth()){
+            	System.out.println("Invalid arguments");
+            	return;
+            }
+            farm.sprayOne(x1,y1);
+            farm.payCoins(20);
+            
+        } else if (tokens.length == 5) {
+            int region[] = parseRegion(tokens);
+            
+            if (region == null) {
+                System.out.println("Invalid arguments");
+                return;
+            }
+            
+            int x1=Integer.parseInt(tokens[1]);
+            int y1=Integer.parseInt(tokens[2]);
+            int x2=Integer.parseInt(tokens[3]);
+            int y2=Integer.parseInt(tokens[4]);
+            
+            if(x1<1||y1<1||x1>farm.getHeight()||y1>farm.getWidth()){
+                if(x2<x1||y2<y1||x2>farm.getHeight()||y2>farm.getWidth()){
+	            	System.out.println("Invalid arguments");
+	            	return;
+                }
+            }
+            if (farm.payCoins((x2-x1+1)*(y2-y1+1)*20)){
+            	for (int i=x1;i<=x2;i++){
+                 	for (int j=y1;j<=y2;j++){
+                         farm.sprayOne(j,i);
+                 	}
+                 }
+            } else {
+            	System.out.println("Not enough coins");
+            	return;
+            }
+
+        } else {
+            System.out.println("Invalid arguments");
+        }
     }
     
     // Infests a crop or region of crop plots
     public static void infestCommand(String[] tokens) {
-        
-        // INFEST <x1> <y1> [<x2> <y2>]
-        
-        System.out.println("TODO");
+                if (tokens.length == 3) {
+            int plot[] = parsePlot(tokens);
+    
+            if (plot == null) {
+                System.out.println("Invalid arguments");
+                return;
+            }
+    
+            int x1=Integer.parseInt(tokens[1]);
+            int y1=Integer.parseInt(tokens[2]);
+            if(x1<1||y1<1||x1>farm.getHeight()||y1>farm.getWidth()){
+            	System.out.println("Invalid arguments");
+            	return;
+            }
+            
+            if (!farm.crops[x1-1][y1-1].isInfested()){
+                farm.payCoins(-20);
+            }
+            farm.infestOne(x1,y1);
+            
+        } else if (tokens.length == 5) {
+            int region[] = parseRegion(tokens);
+            
+            if (region == null) {
+                System.out.println("Invalid arguments");
+                return;
+            }
+            
+            int x1=Integer.parseInt(tokens[1]);
+            int y1=Integer.parseInt(tokens[2]);
+            int x2=Integer.parseInt(tokens[3]);
+            int y2=Integer.parseInt(tokens[4]);
+            
+            if(x1<1||y1<1||x1>farm.getHeight()||y1>farm.getWidth()){
+                if(x2<x1||y2<y1||x2>farm.getHeight()||y2>farm.getWidth()){
+	            	System.out.println("Invalid arguments");
+	            	return;
+                }
+            }
+            
+            int alreadyInfested=0;
+        	for (int i=x1;i<=x2;i++){
+             	for (int j=y1;j<=y2;j++){
+             	    if (farm.crops[i-1][j-1].isInfested()){
+             	        alreadyInfested++;
+             	    }
+                     farm.infestOne(j,i);
+             	}
+             }
+             farm.payCoins(((x2-x1+1)*(y2-y1+1)-alreadyInfested)*-20);
+
+        } else {
+            System.out.println("Invalid arguments");
+        }
     } 
     
     // Harvests a crop plot or region of crop plots
     public static void harvestCommand(String[] tokens) {
-        
-        // HARVEST <x1> <y1> [<x2> <y2>]
-        
-        System.out.println("TODO");
+        if (tokens.length == 3) {
+            int plot[] = parsePlot(tokens);
+    
+            if (plot == null) {
+                System.out.println("Invalid arguments");
+                return;
+            }
+    
+            int x1=Integer.parseInt(tokens[1]);
+            int y1=Integer.parseInt(tokens[2]);
+            if(x1<1||y1<1||x1>farm.getHeight()||y1>farm.getWidth()){
+            	System.out.println("Invalid arguments");
+            	return;
+            }
+            if (farm.crops[x1-1][y1-1].getHeight()>=1&&!farm.crops[x1-1][y1-1].isInfested()){
+                farm.payCoins(-10*farm.crops[x1-1][y1-1].getHeight());
+                farm.harvestOne(x1,y1);
+            }
+            
+        } else if (tokens.length == 5) {
+            int region[] = parseRegion(tokens);
+            
+            if (region == null) {
+                System.out.println("Invalid arguments");
+                return;
+            }
+            
+            int x1=Integer.parseInt(tokens[1]);
+            int y1=Integer.parseInt(tokens[2]);
+            int x2=Integer.parseInt(tokens[3]);
+            int y2=Integer.parseInt(tokens[4]);
+            
+            if(x1<1||y1<1||x1>farm.getHeight()||y1>farm.getWidth()){
+                if(x2<x1||y2<y1||x2>farm.getHeight()||y2>farm.getWidth()){
+	            	System.out.println("Invalid arguments");
+	            	return;
+                }
+            }
+            int amountHarvest=0;
+            
+        	for (int i=x1;i<=x2;i++){
+             	for (int j=y1;j<=y2;j++){
+             	    if (farm.crops[i-1][j-1].getHeight()>=1&&!farm.crops[i-1][j-1].isInfested()){
+                     amountHarvest+=10*farm.crops[i-1][j-1].getHeight();
+                     farm.harvestOne(j,i);
+             	    }
+             	}
+             }
+            farm.payCoins(-amountHarvest);
+
+        } else {
+            System.out.println("Invalid arguments");
+        }
     }
     
     // Attempts to parse plot coordinate
